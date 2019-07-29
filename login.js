1490e24a5a75504516cc1230e628da56bf495f03
bethanyabebe <52415613+bethanyabebe@users.noreply.github.com>
2019-07-29 13:37:10 -0500

    Add files via upload

---
+++ login.js
@@ -0,0 +1,14 @@
+if (password2 != password) {
+    alert("Please reenter password");
+    password.value= "";
+    password2.value= "";
+}
+
+/*
+else {
+  alert("Username saved")
+  saveItemToDatabasez(users)
+  users.value= "";
+  user2.value= "";
+}
+*/
