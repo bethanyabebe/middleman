c39fed8a87bf3f0f54442a2f5c7a436db26e3547
bethanyabebe <52415613+bethanyabebe@users.noreply.github.com>
2019-08-02 17:50:40 -0500

    Update login.js

--- login.js
+++ login.js
@@ -1,14 +1,67 @@
-if (password2 != password) {
-    alert("Please reenter password");
-    password.value= "";
-    password2.value= "";
+function myMenu() {
+  document.getElementById("myDropdown").classList.toggle("show");
 }
 
-/*
-else {
-  alert("Username saved")
-  saveItemToDatabasez(users)
-  users.value= "";
-  user2.value= "";
+window.onclick = function(event) {
+  if (!event.target.matches('.button')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
 }
-*/
+
+
+
+
+$("#signuppage").submit(function(e) {
+  e.preventDefault();
+  
+if(document.getElementById('mentee').checked) {
+  window.location.href = "setupmentee.html";
+  }
+
+  if(document.getElementById('mentor').checked){
+  window.location.href = "setupmentor.html";
+  }  
+
+
+firebase.auth().onAuthStateChanged(firebaseUser => {
+if (firebaseUser) {
+  console.log(firebaseUser); 
+ // btnLogout.classList.remove("hide")
+}
+else {
+  console.log('not logged in')
+//  btnLogout.classList.add("hide")
+}
+})
+})
+
+
+
+$("#loginpage").submit(function(e) {
+  e.preventDefault();
+  const emailInput= document.getElementById("emailInput");
+  const passInput= document.getElementById("passInput");
+  const login= document.getElementById("login");
+
+
+login.addEventListener("click", e => {
+  const email= emailInput.value;
+  const password= passInput.value;
+  const auth = firebase.auth();
+
+  const check1 = auth.signInWithEmailAndPassword(email, password);
+  check1.catch(e => alert(e.message))
+
+ /* btnLogout.addEventListener("click", e => {
+    firebase.auth().signOut();
+  }) */
+
+})
+})
