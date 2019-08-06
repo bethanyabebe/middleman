77d3f3ebd5c026daeb96e427124b012ceaf1bf8f
bethanyabebe <52415613+bethanyabebe@users.noreply.github.com>
2019-08-06 10:15:31 -0500

    Update homepage.js

--- homepage.js
+++ homepage.js
@@ -17,6 +17,30 @@ window.onclick = function(event) {
     }
   }
 }
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function logout() {
+  document.getElementById("myDropdown2").classList.toggle("show2");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(events) {
+  console.log("jp");
+  if (!events.target.matches('.button2')) {
+    console.log("hekko");
+    var dropdownz = document.getElementsByClassName("dropdown2-content");
+    var j;
+    for (j = 0; j < dropdownz.length; j++) {
+      var open = dropdownz[j];
+      if (open.classList.contains('show2')) {
+        console.log("456")
+        open.classList.remove('show2');
+      }
+    }
+  }
+}
+
 /* allows for logout function
 btnLogout.addEventListener("click", e=> {
  firebase.auth().signOut();
