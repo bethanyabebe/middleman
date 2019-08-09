b4ffa8123e2e7d1d34b91e13176b08335d0927e0
bethanyabebe <52415613+bethanyabebe@users.noreply.github.com>
2019-08-08 21:40:02 -0500

    Add files via upload

---
+++ mentor.js
@@ -0,0 +1,41 @@
+const btnLogout = document.getElementById('btnLogout');
+var profile = document.getElementById('profile');
+var log = document.getElementById('login2');
+
+
+btnLogout.addEventListener('click', e => {
+    firebase.auth().signOut();
+    }); 
+
+  firebase.auth().onAuthStateChanged(firebaseUser => {
+       if (firebaseUser) {
+           console.log(firebaseUser); 
+           log.style.display= 'none';
+           profile.style.display= 'hidden';
+              }
+              else {
+           console.log('not logged in')
+           log.style.display= 'hidden';
+           profile.style.display= 'none';
+                  }
+              })
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById('myDropdown2').classList.toggle('show2')
+}
+
+// Close the dropdown menu if the user clicks outside of it
+$('#button2').click(function(event2) {
+  if (!event2.target.matches('.button2')) {
+    var dropdownz = document.getElementsByClassName('dropdown2-content')
+    var j
+    for (j = 0; j < dropdownz.length; j++) {
+      var open = dropdownz[j]
+      if (open.classList.contains('show2')) {
+        open.classList.remove('show2')
+      }
+    }
+  }
+});
